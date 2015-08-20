131d3ffea9891c24eda154e72f0808e9065b5d73
tsde <havetobeunknown@gmail.com>
2015-08-20 11:18:28 +0200

    Fix indent and change tabs to spaces

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ if (!process.env.LOG4JS_CONFIG
  || !process.env.RD_API_TOKEN
  || !process.env.RD_API_JOB_ENV_ID
  || !process.env.RD_API_JOB_MOD_ID) {
-	logger.fatal('!! OOOPS !! The following environment variables are mandatory:\n\t- LOG4JS_CONFIG: Path to your logger configuration file\n\t- PUPPETFILE_URL: Your Puppetfile git repository URL\n\t- RD_BASEURL: Rundeck base URL\n\t- RD_API_VERSION: Version number of Rundeck API\n\t- RD_API_TOKEN: Token used to access Rundeck API\n\t- RD_PROJECT_NAME: Name of your Rundeck project where are configured your job\n\t- RD_API_JOB_ENV_ID: ID of the job used to deploy entire environments with r10k\n\t- RD_API_JOB_MOD_ID: ID of the job used to deploy a single module in a specific environment with r10k');
-	process.exit(1);
+  logger.fatal('!! OOOPS !! The following environment variables are mandatory:\n\t- LOG4JS_CONFIG: Path to your logger configuration file\n\t- PUPPETFILE_URL: Your Puppetfile git repository URL\n\t- RD_BASEURL: Rundeck base URL\n\t- RD_API_VERSION: Version number of Rundeck API\n\t- RD_API_TOKEN: Token used to access Rundeck API\n\t- RD_PROJECT_NAME: Name of your Rundeck project where are configured your job\n\t- RD_API_JOB_ENV_ID: ID of the job used to deploy entire environments with r10k\n\t- RD_API_JOB_MOD_ID: ID of the job used to deploy a single module in a specific environment with r10k');
+  process.exit(1);
 }
 
 var app = koa();
@@ -31,31 +31,31 @@ router.post('/gitlab', plugin.gitlabProcess());
 
 // Use middlewares
 app
-	.use(plugin.pageNotFound())
-	.use(router.routes())
-	.use(router.allowedMethods());
+  .use(plugin.pageNotFound())
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 // Start Koa app with SSL support if wanted
-var sslRequired = (process.env.KOA_USE_SSL === "true");
+var sslRequired = (process.env.KOA_USE_SSL === 'true');
 
 if (sslRequired) {
 
-	var key_file = process.env.KOA_SSL_KEY || 'examples/ssl/r10k-deployerjs.key';
-	var cert_file = process.env.KOA_SSL_CERT || 'examples/ssl/r10k-deployerjs.crt';
+  var key_file = process.env.KOA_SSL_KEY || 'examples/ssl/r10k-deployerjs.key';
+  var cert_file = process.env.KOA_SSL_CERT || 'examples/ssl/r10k-deployerjs.crt';
 
-	var ssl_opts = {
-		key: fs.readFileSync(key_file),
-		cert: fs.readFileSync(cert_file)
-	}
+  var ssl_opts = {
+    key: fs.readFileSync(key_file),
+    cert: fs.readFileSync(cert_file)
+  };
 
-	// HTTPS
-	https.createServer(ssl_opts, app.callback()).listen(app_port);
+  // HTTPS
+  https.createServer(ssl_opts, app.callback()).listen(app_port);
 
 }
 else {
 
-	// HTTP
-	app.listen(app_port);
+  // HTTP
+  app.listen(app_port);
 
 }
 
